0e3b2f8364087524f1f4ea52df9254084718dc58
progfay <shun1856shun@gmail.com>
2020-07-29 00:03:30 +0900

    feat: add case for Temporal.Date.from

--- index.js
+++ index.js
@@ -10,24 +10,24 @@ const { Temporal } = pkg
 
 console.group('Temporal.Date.from')
 
-const [year, month, day] = [20000, 1000000,10000]
+for (const [year, month, day] of [[2000, 13, 32], [20000, 100, 100]]) {
+  console.group(`Temporal.Date.from('${year}-${month}-${day}')`)
+  try {
+    const date = Temporal.Date.from(`${year}-${month}-${day}`)
+    console.log(date)
+  } catch (e) {
+    console.error(e)
+  }
+  console.groupEnd(`Temporal.Date.from('${year}-${month}-${day}')`)
 
-console.group(`Temporal.Date.from('${year}-${month}-${day}')`)
-try {
-  const date = Temporal.Date.from(`${year}-${month}-${day}`)
-  console.log(date)
-} catch (e) {
-  console.error(e)
+  console.group(`Temporal.Date.from({ year: ${year}, month: ${month}, day: ${day} })`)
+  try {
+    const date = Temporal.Date.from({ year, month, day })
+    console.log(date)
+  } catch (e) {
+    console.error(e)
+  }
+  console.groupEnd(`Temporal.Date.from({ year: ${year}, month: ${month}, day: ${day} })`)
 }
-console.groupEnd(`Temporal.Date.from('${year}-${month}-${day}')`)
-
-console.group(`Temporal.Date.from({ year: ${year}, month: ${month}, day: ${day} })`)
-try {
-  const date = Temporal.Date.from({ year, month, day })
-  console.log(date)
-} catch (e) {
-  console.error(e)
-}
-console.groupEnd(`Temporal.Date.from({ year: ${year}, month: ${month}, day: ${day} })`)
 
 console.groupEnd('Temporal.Date.from')
